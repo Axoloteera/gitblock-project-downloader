c769428ac17bf3d6c01fb26d90f31c63257ee300
Gtd232 <im.gtd232@outlook.com>
2024-02-10 17:54:49 +0800

    支持了下载作品json

--- gitblockProjectDownloader.py
+++ gitblockProjectDownloader.py
@@ -5,10 +5,19 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 import base64
+from urllib.request import Request, urlopen
 
-def aes_decrypt(text):
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+
+def aes_decrypt(bytesText: bytes) -> str:
     # CBC
-    return unpad(AES.new(b'4A9745825F24883B657AFC4E4626A0F2', AES.MODE_CBC, b'4A9745825F24883B').decrypt(base64.b64decode(text.encode())),AES.block_size,'pkcs7').decode('utf-8')
+    return unpad(AES.new(b'4A9745825F24883B657AFC4E4626A0F2', AES.MODE_CBC, b'4A9745825F24883B').decrypt(bytesText), AES.block_size, style='pkcs7').decode('utf-8')
+
+def download_json(id:int,ver:int,https:bool=True) -> str:
+    return aes_decrypt(urlopen(Request(f"http{'s' if https else ''}://asset.gitblock.cn/Project/download?id={id}&v={ver}",headers=headers)).read())
+
 
 
 
