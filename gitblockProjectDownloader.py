b7268d1f7c6300fa1cad098fa1d0b15dee643cdd
Gtd232 <im.gtd232@outlook.com>
2024-02-10 18:37:30 +0800

    支持了下载sb3文件,且修改了素材的下载函数,并增加下载至内存

--- gitblockProjectDownloader.py
+++ gitblockProjectDownloader.py
@@ -8,6 +8,7 @@ import base64
 from urllib.request import Request, urlopen
 import json
 import os
+import zipfile
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
@@ -20,24 +21,53 @@ def aes_decrypt(bytesText: bytes) -> str:
 def download_json(id:int,ver:int,https:bool=True) -> str:
     return aes_decrypt(urlopen(Request(f"http{'s' if https else ''}://asset.gitblock.cn/Project/download?id={id}&v={ver}",headers=headers)).read())
 
-def download_assets(prjJson:str, path:str="./assets", https:bool=True) -> bool:
+def download_assets(prjJson:str, path:str="./assets", https:bool=True) -> list:
     if not os.path.exists(path):
         os.makedirs(path)
     prj = json.loads(prjJson)
+    assets = []
     for sprite in prj['targets']:
         for asset in sprite['costumes']:
+            assets.append(asset['md5ext'])
             with open(f"{path}/{asset['md5ext']}", 'wb') as f:
                 try:
                     f.write(urlopen(Request(f"http{'s' if https else ''}://cdn.gitblock.cn/Project/GetAsset?name={asset['md5ext']}", headers=headers)).read())
                 except:
-                    return False
+                    raise 'Download Error!'
         for asset in sprite['sounds']:
+            assets.append(asset['md5ext'])
             with open(f"{path}/{asset['md5ext']}", 'wb') as f:
                 try:
                     f.write(urlopen(Request(f"http{'s' if https else ''}://cdn.gitblock.cn/Project/GetAsset?name={asset['md5ext']}", headers=headers)).read())
                 except:
-                    return False
-    return True
+                    raise 'Download Error!'
+    return assets
+
+def download_assets_memory(prjJson:str, https:bool=True) -> dict:
+    prj = json.loads(prjJson)
+    assets = {}
+    for sprite in prj['targets']:
+        for asset in sprite['costumes']:
+            try:
+                assets[asset['md5ext']] = urlopen(Request(f"http{'s' if https else ''}://cdn.gitblock.cn/Project/GetAsset?name={asset['md5ext']}", headers=headers)).read()
+            except:
+                raise 'Download Error!'
+        for asset in sprite['sounds']:
+            try:
+                assets[asset['md5ext']] = urlopen(Request(f"http{'s' if https else ''}://cdn.gitblock.cn/Project/GetAsset?name={asset['md5ext']}", headers=headers)).read()
+            except:
+                raise 'Download Error!'
+    return assets
+
+def download_sb3(id:int,ver:int, fileName:str="project.sb3",https:bool=True) -> None:
+    prjJson = download_json(id,ver,https)
+    assets = download_assets_memory(prjJson,https=https)
+    with zipfile.ZipFile(fileName, mode="w") as archive:
+        for i in assets:
+            with archive.open(i, "w") as f:
+                f.write(assets[i])
+        archive.writestr("project.json", prjJson)
+
 
 
 
